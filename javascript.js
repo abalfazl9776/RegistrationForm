d4d045e2c2cdb317a82547e48808597792d8b0d8
Abolfazl Vayani <avhb2lr@gmail.com>
2019-12-21 17:15:03 +0330

    initial

---
+++ javascript.js
@@ -0,0 +1,65 @@
+tippy('#firstname', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#firstname', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#lastname', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#username', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#password', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#passwordRetype', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#day', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#month', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#year', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#gender', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
+
+tippy('#phone', {
+    content: "I'm a Tippy tooltip!",
+    placement: "right",
+    trigger: "focus"
+});
